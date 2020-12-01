2813536585f03f23a7178edb2cd9b490c30f12c2
Lewis Dale <lewis.dale@fortacode.co.uk>
2020-12-01 18:23:02 +0000

    Implementation for day one

--- src/report/report.test.ts
+++ src/report/report.test.ts
@@ -1,14 +1,40 @@
 const { report } = require('./report');
 
-test('Generates the output for a report', () => {
-    const expenses = [
-        1721,
-        979,
-        366,
-        299,
-        675,
-        1456,
-    ];
-    const result = report(expenses);
-    expect(result).toBe(514579);
+describe('Report', () => {
+    test('Generates the output for the default report', () => {
+        const expenses = [
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        ];
+        const result = report(expenses);
+        expect(result).toBe(514579);
+    });
+
+    test('Returns the correct output if two inputs add up to 2020', () => {
+        const expenses = [
+            1510,
+            979,
+            366,
+            510,
+            675,
+            1456,
+        ];
+        const result = report(expenses);
+        expect(result).toBe(770100);
+    });
+
+    test('Returns 0 if no two inputs add up to 2020', () => {
+        const expenses = [
+            200,
+            800,
+            4000,
+            120,
+        ];
+        const result = report(expenses);
+        expect(result).toBe(0);
+    });
 });
--- src/report/report.ts
+++ src/report/report.ts
@@ -1,1 +1,27 @@
-export const report = () : number => 514579;
+export const report = (expenses : Array<number>) : number => {
+    const pairs = permutations(expenses)
+        .filter(([x, y]) => (x+y) === 2020);
+
+    if (!pairs.length) {
+        return 0;
+    }
+
+    return pairs[0][0] * pairs[0][1];
+}
+
+const permutations = (list : Array<any>) : Array<Array<any>> => {
+    if (!list.length) {
+        return [];
+    }
+    if (list.length === 1) {
+        return [[list]];
+    }
+
+    let permutations = [];
+    for (let i = 0; i < list.length-1; i++) {
+        for (let j = i+1; j < list.length; j++) {
+            permutations.push([list[i], list[j]]);
+        }
+    }
+    return permutations;
+}
