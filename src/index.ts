1522280ce0fee2a18d0273327007b7fb00d29ef7
Lewis Dale <lewis.dale@fortacode.co.uk>
2020-12-01 18:29:38 +0000

    Day one part one

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import fs from 'fs';
+
+import { report } from './report';
+
+const expenses = fs.readFileSync('./inputs/day-one.txt')
+    .toString('utf-8')
+    .split('\n')
+    .map(str => parseInt(str));
+
+const reportOutput = report(expenses);
+console.log(reportOutput);
