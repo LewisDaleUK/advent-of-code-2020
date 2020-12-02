45506f2235b596fce555d9c13523539be0c7680d
Lewis Dale <lewis.dale@fortacode.co.uk>
2020-12-02 21:32:34 +0000

    Modified day one for part two

--- src/report/report.ts
+++ src/report/report.ts
@@ -1,26 +1,28 @@
 export const report = (expenses : Array<number>) : number => {
     const pairs = permutations(expenses)
-        .filter(([x, y]) => (x+y) === 2020);
+        .filter(([x, y, z]) => (x+y+z) === 2020);
 
     if (!pairs.length) {
         return 0;
     }
 
-    return pairs[0][0] * pairs[0][1];
+    return pairs[0][0] * pairs[0][1] * pairs[0][2];
 }
 
 const permutations = (list : Array<any>) : Array<Array<any>> => {
     if (!list.length) {
         return [];
     }
-    if (list.length === 1) {
-        return [[list]];
+    if (list.length === 1 || list.length === 2) {
+        return [list];
     }
 
     let permutations = [];
-    for (let i = 0; i < list.length-1; i++) {
-        for (let j = i+1; j < list.length; j++) {
-            permutations.push([list[i], list[j]]);
+    for (let i = 0; i < list.length-2; i++) {
+        for (let j = i+1; j < list.length-1; j++) {
+            for (let k = j+1; k < list.length; k++) {
+                permutations.push([list[i], list[j], list[k]]);
+            }
         }
     }
     return permutations;
